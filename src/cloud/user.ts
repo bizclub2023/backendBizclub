27abf87924648ae0e3c14feb21051f483abad814
Golfredo Perez Fernandez <golfredo.pf@gmail.com>
2023-11-21 16:38:11 -0400

    asd

--- src/cloud/user.ts
+++ src/cloud/user.ts
@@ -66,8 +66,84 @@ Parse.Cloud.define("getSalon", async (request: any) => {
   // Devuelve el 'salon' asociado al usuario
   return salon;
 });
+let list = [
+  {
+    id: 'ed2b900870ceba72d203ec15',
+    createdAt: '31/03/2019',
+    description: 'Acceso a espacios de coworking compartidos durante el horario comercial, con serviciosbásicos y conexión a intemet de alta velocidad.',
+    logo: 'https://bafybeidwnhdez3yjg6w7gpg7esgtzicbj3l5sqehepd5xng3xafegybpfu.ipfs.nftstorage.link/tecnologia.png',
+    title: 'Emprendedor Express',
+    price: '250.000 COP/MES'
+  },
+  {
+    id: 'a033e38768c82fca90df3db7',
+    createdAt: '03/04/2019',
+    description: 'Acceso 12/6 a espacios de coworking compartidos, con servicios básicos y conexión a intenet de alta velocidad. Incluye un número limitado de horas en salas de reuniones y acceso a eventos de networking.',
+    logo: 'https://bafybeidwnhdez3yjg6w7gpg7esgtzicbj3l5sqehepd5xng3xafegybpfu.ipfs.nftstorage.link/logistic.png',
+    title: 'Visionario Flexible',
+    price: '380.000 COP/MES'
+  },
+  {
+    id: '1efecb2bf6a51def9869ab0f',
+    createdAt: '04/04/2019',
+    description: 'Escritorio dedicado y acceso 12/6 a espacios de coworking, con servicios básicos y conexión a intenet de alta velocidad. Incluye un número mayor de horas en salas de reuniones, acceso a eventos de networking y descuentos en servicios adicionales.',
+    logo: 'https://bafybeidwnhdez3yjg6w7gpg7esgtzicbj3l5sqehepd5xng3xafegybpfu.ipfs.nftstorage.link/money-management.png',
+    title: 'Innovador Dedicado',
+    price: '650.000 COP/MES'
+  },
+  {
+    id: '1ed68149f65fbc6089b5fd07',
+    createdAt: '04/04/2019',
+    description: 'Oficina privada con acceso 12/6, servicios básicos y conexión a intenet de alta velocidad. Incluye un número ilimitado de horas en salas de reuniones, acceso a eventos de networking, descuentos en servicios adicionales y asesoría en áreas de negocio.',
+    logo: 'https://bafybeidwnhdez3yjg6w7gpg7esgtzicbj3l5sqehepd5xng3xafegybpfu.ipfs.nftstorage.link/reunion.png',
+    title: 'Líder Elite',
+    price: '900.000 COP/MES'
+  },
+  {
+    id: '5dab321376eff6177407e887',
+    createdAt: '04/04/2019',
+    description: 'Membresía para empresas, incluye oficinas privadas, espacios de coworking, acceso a salas de reuniones y eventos de networking. Servicios adicionales como asesoría en áreas de negocio, mentorías y soporte administrativo están incluidos.',
+    logo: 'https://bafybeidwnhdez3yjg6w7gpg7esgtzicbj3l5sqehepd5xng3xafegybpfu.ipfs.nftstorage.link/trabajando.png',
+    title: 'Corporativo Vanguardista',
+    price: '3.000.000 COP/MES'
+  },
+  {
+    id: '5dab321376eff6177407e887',
+    createdAt: '04/04/2019',
+    description: 'Acceso a todas las instalaciones y servicios del club, incluyendo áreas de trabajo exclusivas, salas de reuniones premium, eventos de networking, asesoría en áreas de negocio, mentorías y acceso a instalaciones y servicios de lujo.',
+    logo: 'https://bafybeidwnhdez3yjg6w7gpg7esgtzicbj3l5sqehepd5xng3xafegybpfu.ipfs.nftstorage.link/megafono.png',
+    title: 'Titán del Éxito',
+    price: '1.500.000 COP/MES'
+  }
+];
 
+Parse.Cloud.define("fetchCoworking", async (request: any) => {
+  let empty:any=[]
+  
+  
+      for(let i=0;i<list.length;i++){
+  
+        const query = new Parse.Query("_User");
+  
+        await query.equalTo("planName",list[i].title)     
+       await query.equalTo("planActive",true)     
+       let object= await query.find()
+  let numberSusbcription=object.length
+  
+  empty=[...empty,{
+          
+    id: i,
+    createdAt: list[i].createdAt,
+    description: list[i].description,
+    logo: list[i].logo,
+    title: list[i].title,
+    price: list[i].price,
+    avaliable:numberSusbcription,
+      }]
+      }
 
+      return empty
+});
 
 Parse.Cloud.define("getEventsAdmin", async (request: any) => {
   const query = new Parse.Query("Reserves");
